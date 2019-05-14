2ca6dc3f1e85cc6adb00ca8ce31ca7980ced7a1b
Tim O'Grady <Tim.OGrady@anthonynolan.org>
2019-05-14 12:07:22 +0100

    added dapper and employee class

---
+++ Employment/Employment/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+
+namespace Employment
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public decimal PensionFundTotal { get; set; }
+        public decimal ProviderName { get; set; }
+
+        public List<Employee> GetEmployeeList()
+        {
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["EmploymentConnection"].ConnectionString);
+            string Sqlstring = "Select * from employees_with_pensions";
+            var employees = (List <Employee>)db.Query<Employee>(Sqlstring);
+            return employees;
+        }
+    }
+
+ 
+}
