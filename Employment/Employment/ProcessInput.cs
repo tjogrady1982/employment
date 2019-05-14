5dbc8abf6372515b10e8d97c74da33433b677e60
Tim O'Grady <Tim.OGrady@anthonynolan.org>
2019-05-14 10:40:00 +0100

    simple console app with some validation of inputs

---
+++ Employment/Employment/ProcessInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employment
+{
+    public class ProcessInput
+    {
+        public string ProcessMessage(int value)
+        {
+            if (value == 1)
+            {
+                return "List of Employees";
+            }
+
+            else if (value == 2)
+            {
+                return "Adding 5% of Salary to pension fund";
+            }
+            else
+
+            {
+                return "This is not a valid input. Please select either 1 or 2";
+            }
+        }
+    }
+}
---
+++ Employment/Employment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().RunEmploymentApp();
+        }
+
+        private void RunEmploymentApp()
+        {
+            Console.WriteLine("Welcome to the Employment Database");
+            Console.WriteLine("Please select from the following options by keying the relevant number and pressing enter:");
+            Console.WriteLine();
+            Console.WriteLine("1: List of employees with their job role, salary and pension fund balance");
+            Console.WriteLine("2: Add 5% of employee salary to the pension fund");
+            Console.WriteLine();
+            string input = Console.ReadLine();
+
+            //check validity of input
+            if (int.TryParse(input, out int value))
+            {
+                var process = new ProcessInput();
+                var outputMessage = process.ProcessMessage(value);
+                Console.WriteLine(outputMessage);
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("This is not a valid input. Please select either 1 or 2");
+                Console.ReadLine();
+            }
+        }
+    }
+}
